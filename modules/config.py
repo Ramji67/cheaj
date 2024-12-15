7fe7b676877eb482df2d50050a75bfa09bbe1554
Ramji67 <158837797+Ramji67@users.noreply.github.com>
2024-12-16 00:04:41 +0530

    config.py

--- modules/config.py
+++ modules/config.py
@@ -1,6 +1,6 @@
 # config.py
 BOT_NAME = "Free bot"
-BOT_TOKEN = "7676127286:AAGA99obWbphTO9DNO3oNtWyIzM4BQ-e9rw"
+BOT_TOKEN = ""
 API_ID = 28088290
 API_HASH = "6998f2c585fdce65ac72dfa23d02b6ec"
 MONGO_URI = "mongodb+srv://mrnobody:modernhackers@mrnobody.q8e87ij.mongodb.net/?retryWrites=true&w=majority&appName=MrNobody"
