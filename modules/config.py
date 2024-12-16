5d6d21a22d5045a27f1764b96284c1b9fdcb071e
Ramji67 <158837797+Ramji67@users.noreply.github.com>
2024-12-16 06:22:36 +0530

    Delete modules directory

--- modules/config.py
+++
@@ -1,6 +0,0 @@
-# config.py
-BOT_NAME = "Free bot"
-BOT_TOKEN = ""
-API_ID = 28088290
-API_HASH = "6998f2c585fdce65ac72dfa23d02b6ec"
-MONGO_URI = "mongodb+srv://mrnobody:modernhackers@mrnobody.q8e87ij.mongodb.net/?retryWrites=true&w=majority&appName=MrNobody"
